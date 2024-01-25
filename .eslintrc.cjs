92bc62678eb80db3090e2d7bb3ffacd18beac0f3
Martin <mr.mmaurer@gmail.com>
2024-01-25 18:19:46 +0000

    Migrated to nextjs and added project config files

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:perfectionist/recommended-natural',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'next',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    'jsx-a11y/label-has-associated-control': 'off'
+  },
+};
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grandwoof-barking
 Live long and prosper.
+
+Trying out mixing React Query/TanStack Query with Server actions. Initial fetching happens server-side and subsequent fetches happen client side
