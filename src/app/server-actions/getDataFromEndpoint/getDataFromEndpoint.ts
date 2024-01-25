f25c8a9e54f786a8ac14d8127aa3607023c9c49c
Martin <mr.mmaurer@gmail.com>
2024-01-25 23:06:15 +0000

    Added 404 page

--- src/app/server-actions/getDataFromEndpoint/getDataFromEndpoint.ts
+++ src/app/server-actions/getDataFromEndpoint/getDataFromEndpoint.ts
@@ -2,9 +2,11 @@
 
 // taken from https://github.com/maurer2/blue-harvest/blob/main/src/app/server-actions/getDataFromEndpoint/getDataFromEndpoint.ts
 export const getDataFromEndpoint = async <T>(url: string): Promise<T> => {
-  const fullURL = new URL(url);
+  const response = await fetch(url);
 
-  const response = await fetch(fullURL.toString());
+  if (!response.ok) {
+    throw new Error(response.statusText || `Error fetching from ${url}}`);
+  }
 
   return response.json() as Promise<T>;
 };
